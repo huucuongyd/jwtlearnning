2dce6e94063d74579473bdd7832be3b7f4c4238c
vinhmh <vinh.opentechiz@gmail.com>
2023-04-06 17:40:57 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,32 +1,33 @@
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const salt = await bcrypt.genSalt(saltRounds);
 const db = [
   {
-    username:"abc",
-    password:"123"
+    username: "abc",
+    password: "123",
+    paswordHassed:
+      "$2b$10$N4vBK5pbfLy82Ag7JxoI4OZxgLRLuKY7tXx0ehg9SN30F9OcQ5WEK",
   },
   {
-    username:"xyz",
-    password:"888"
-  }
-]
+    username: "xyz",
+    password: "888",
+    paswordHassed:
+      "$2b$10$N4vBK5pbfLy82Ag7JxoI4OoDRt47qWpLTMX1V.VvVFlqvRf0GYw6S",
+  },
+];
 
-let dbhash = []
+let dbhash = [];
 
-const hassP = async (db) => {
-  db.forEach(e => {
-    let passhass = bcrypt.hash(e.password,salt)
-  });
-}
+const login = async (un, pw) => {
+  // check un exist in array db
 
-const login = async (pw) => {
-  
+  // if un exist continue check password
+  const salt = await bcrypt.genSalt(saltRounds);
   const hash = await bcrypt.hash(myPlaintextPassword, salt);
-  const cpvl = await bcrypt.hash(pw,salt)
-  return hash === cpvl
-//   return await bcrypt.compare(pw, hash);
+  const cpvl = await bcrypt.hash(pw, salt);
+  return hash === cpvl;
+  //   return await bcrypt.compare(pw, hash);
 };
 (async () => {
-  console.log(await login("s0//P4$$w0rD"));
+  // console.log(await login("s0//P4$$w0rD"));
+  await hassP(db);
 })();
